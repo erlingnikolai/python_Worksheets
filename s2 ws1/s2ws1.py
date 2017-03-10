ce89eb622b6770c3f143b7363e2b07459b1d96c4
Unknown <erlingnu@gmail.com>
2017-03-10 10:49:01 +0000

    Name changes

--- s2 ws1/s2ws1.py
+++ s2 ws1/s2ws1.py
@@ -39,7 +39,7 @@ def ws10five():
         print(deck2)
 
 
-def ws10six():
+def ws10six(): # not working 100%
     deck1 = 'CA C2 C3 C4 C5 C6 C7 C8 C9 C10 CJ CQ CK DA D2 D3 D4 D5 D6 D7 D8 D9 D10 DJ DQ DK HA H2 H3 H4 H5 H6 H7 H8 ' \
             'H9 H10 HJ HQ HK SA S2 S3 S4 S5 S6 S7 S8 S9 S10 SJ SQ SK'.split()
     player1 = r.sample(deck1, k=5)
