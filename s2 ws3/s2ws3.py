75b321429180998c9d398cc2fa8e9c6324ecde8b
Unknown <erlingnu@gmail.com>
2017-03-10 11:59:29 +0000

    new worksheet added

---
+++ s2 ws3/s2ws3.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random as r
+
+import pip
+import scipy.stats as stats
+intArray = np.random.randint(10, high= 100, size=10)
+print("numbers = " , intArray)
+print("median = " , np.median (intArray))
+print("mean = " , np.mean (intArray))
+print("standard deviation = ", np.std (intArray))
+
