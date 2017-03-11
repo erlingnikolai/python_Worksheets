f1f0d220a328729a58ecaf4272c0a32bcefdb462
Unknown <erlingnu@gmail.com>
2017-03-11 10:15:44 +0000

    ws4 maybe done

--- s2 ws4/s2ws4.py
+++ s2 ws4/s2ws4.py
@@ -5,34 +5,23 @@ url = "http://wps.aw.com/wps/media/objects/14/15269/projects/ch6_iq/brain.txt"
 
 df = pd.read_table(url, sep ="\t")
 female = df[df["Gender"] == "Female"]
-a = df.loc[:, "Height"]
-retard = []
-for x in a:
-    if math.isnan(x):
-        print(x)
-    else:
-        retard.append(x)
-#print(a.describe())
-
-tihi =stats.ttest_1samp(a =retard, popmean =163)
-print(tihi)
+male = df[df["Gender"] == "Male"]
+a = female.loc[:, "Height"]
+b = male.loc[:, "Height"]
 
 
+def find_info(temp):
+    something = []
+    for x in temp:
+        if not math.isnan(x):
+            something.append(x)
+    return something
 
 
+test_male = stats.ttest_1samp(a=find_info(b), popmean=170)
+test_female = stats.ttest_1samp(a=find_info(a), popmean=163)
+test_compare = stats.ttest_ind(a=find_info(a), b=find_info(b))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(test_male)
+print(test_female)
+print(test_compare)
